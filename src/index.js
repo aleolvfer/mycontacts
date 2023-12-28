978a203657cd9a463567cdfd63ed5e4f7d63a4e6
aleolvfer <aleolvfer@hotmail.com>
2023-12-28 13:47:15 -0300

    Commit inicial

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (request, response) => {
   response.send('Hello World!');
-})
+});
 
 app.listen(3000, () => console.log('Server started at http://localhos:3000'));
